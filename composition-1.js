a7ea561a3b9bd829b9fb3ac88351c3aa856c675e
canIgit <canigit@yahoo.com>
2018-09-07 01:29:15 +0530

    Create composition-1.js

---
+++ composition-1.js
@@ -0,0 +1,31 @@
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+  );
+}
+
+function WelcomeDialog() {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        Welcome
+      </h1>
+      <p className="Dialog-message">
+        Thank you for visiting our spacecraft!
+      </p>
+    </FancyBorder>
+  );
+}
+
+/*Output:
+<div class='FancyBorder FancyBorder-blue>
+    <h1 class="Dialog-title">
+      Welcome
+    </h1>
+    <p class="Dialog-message">
+      Thank you for visiting our spacecraft!
+    </p>
+</div>
+*/
